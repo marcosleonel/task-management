e65ff0265f2e806f35442159c568a8248f5199a9
Marcos Leonel <leonel.coder@gmail.com>
2022-05-15 14:43:32 -0300

    feat: add a logger component

---
+++ src/logger/index.js
@@ -0,0 +1,47 @@
+const path = require('path')
+const { createLogger, format, transports } = require('winston')
+
+const {
+  combine,
+  timestamp,
+  label,
+  printf
+} = format
+
+const logFormat = printf((info) => `${info.timestamp} [${info.label}] ${info.level}: ${info.message}`)
+
+const logger = createLogger({
+  format: combine(
+    format.colorize(),
+    format.json(),
+    label({ label: 'Node.js REST API' }),
+    timestamp(),
+    logFormat
+  ),
+
+  transports: [
+    new (transports.Console)({
+      json: false,
+      timestamp: true
+    }),
+    new transports.File({
+      filename: path.join(__dirname, '/logs/debug.log'),
+      json: false
+    })
+  ],
+
+  exceptionHandlers: [
+    new (transports.Console)({
+      json: false,
+      timestamp: true
+    }),
+    new transports.File({
+      filename: path.join(__dirname, '/logs/exceptions.log'),
+      json: false
+    })
+  ],
+
+  exitOnError: false
+})
+
+module.exports = logger
